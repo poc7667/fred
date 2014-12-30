b8473c38699058245c7d19d1d11345728ad043dc
poc7667 <poc7667@gmail.com>
2014-12-30 11:47:35 +0800

    Support dynamic create any field

--- app/models/indicator.rb
+++ app/models/indicator.rb
@@ -1,6 +1,7 @@
 class Indicator
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Mongoid::Attributes::Dynamic
   field :id, type: String
   field :name, type: String
   field :category, type: String
