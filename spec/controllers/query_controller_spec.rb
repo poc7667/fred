c6402ebbc114b1ac62dc3d5c5cc7f94e387000ad
poc7667 <poc7667@gmail.com>
2014-12-31 21:02:05 +0800

    Add test cases

--- spec/controllers/query_controller_spec.rb
+++ spec/controllers/query_controller_spec.rb
@@ -4,9 +4,31 @@ RSpec.describe QueryController, :type => :controller do
 
   describe "GET index" do
     it "returns http success" do
-      get :index
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "Search for title " do
+    it "returns title" do
+      get :index, {
+        title: "Railroad"
+      }
+    end
+
+    it "find by id" do
+      get :index, {
+        id: "Q10099USQ144NNBR"
+      }
+    end   
+
+    it "find by name"  do
+      get :index, {
+        name: "Total Number of Locomotives Assigned",
+        id: "Q10099USQ144NNBR"
+      }
+    end
+  end
+
+
 
 end
